8a1c06d17b919244f75c0859e81a2b980b9675b8
Tikhon Bernstam <tikhon@gmail.com>
2012-04-18 20:54:48 -0700

    add push support to parse-ruby-client

---
+++ lib/parse/push.rb
@@ -0,0 +1,31 @@
+require 'cgi'
+require 'parse/error'
+
+module Parse
+  class Push
+    attr_accessor :channels
+    attr_accessor :channel
+    attr_accessor :type
+    attr_accessor :expiration_time_interval
+    attr_accessor :expiration_time
+    attr_accessor :data
+
+    def initialize(data, channel = "")
+      @data = data
+      @channel = channel || ""
+    end
+
+    def send
+      uri   = Protocol.push_uri
+      body = { :data => @data, :channel => @channel }
+      body.merge({ :channels => @channels }) if @channels && @channel.nil?
+      body.merge({ :expiration_time_interval => @expiration_time_interval }) if @expiration_time_interval
+      body.merge({ :expiration_time => @expiration_time }) if @expiration_time && @expiration_time_interval.nil?
+      body.merge({ :type => @type }) if @type
+      
+      response = Parse.client.request uri, :post, body.to_json, nil
+    end
+
+  end
+
+end
