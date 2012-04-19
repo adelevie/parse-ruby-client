8d4e262bf13cfe8eec42eb219b459cd46645cc38
Tikhon Bernstam <tikhon@gmail.com>
2012-04-18 21:24:07 -0700

    on second thought, dont save users from bad behavior of setting two conflicting variables

--- lib/parse/push.rb
+++ lib/parse/push.rb
@@ -17,13 +17,11 @@ module Parse
 
     def send
       uri   = Protocol.push_uri
+      
       body = { :data => @data, :channel => @channel }
-      if @channel == "" && @channels
-        body.merge({ :channels => @channels }) 
-        body.delete :channel
-      end
+      body.merge({ :channels => @channels }) if @channels
       body.merge({ :expiration_time_interval => @expiration_time_interval }) if @expiration_time_interval
-      body.merge({ :expiration_time => @expiration_time }) if @expiration_time && @expiration_time_interval.nil?
+      body.merge({ :expiration_time => @expiration_time }) if @expiration_time 
       body.merge({ :type => @type }) if @type
       
       response = Parse.client.request uri, :post, body.to_json, nil
