20446c381d9cda6649c690b2c490302c63baa36d
Eric Jensen <ericcj@gmail.com>
2012-06-29 14:06:06 -0400

    don't delete all reserved keys from ourselves on update where they won't be returned to us from server

--- test/test_client.rb
+++ test/test_client.rb
@@ -4,7 +4,7 @@ class TestClient < Test::Unit::TestCase
   def setup
     Parse.init
   end
-  
+
   def test_simple_save
     test_save = Parse::Object.new "TestSave"
     test_save["foo"] = "bar"
@@ -14,42 +14,48 @@ class TestClient < Test::Unit::TestCase
     assert_equal test_save[Parse::Protocol::KEY_CREATED_AT].class, String
     assert_equal test_save[Parse::Protocol::KEY_OBJECT_ID].class, String
   end
-  
+
   def test_update
     foo = Parse::Object.new "TestSave"
     foo["age"] = 20
     foo.save
-    
+
     assert_equal foo["age"], 20
     assert_equal foo[Parse::Protocol::KEY_UPDATED_AT], nil
-    
+
     foo["age"] = 40
+    orig = foo.dup
     foo.save
-    
+
     assert_equal foo["age"], 40
     assert_equal foo[Parse::Protocol::KEY_UPDATED_AT].class, String
+
+    # only difference should be updatedAt
+    orig_assoc = orig.reject{|k,v| k == Parse::Protocol::KEY_UPDATED_AT}.to_a
+    foo_assoc = foo.reject{|k,v| k == Parse::Protocol::KEY_UPDATED_AT}.to_a
+    assert_equal foo_assoc, orig_assoc
   end
-  
+
   def test_server_update
-    foo = Parse::Object.new("TestSave").save 
+    foo = Parse::Object.new("TestSave").save
     foo["name"] = 'john'
     foo.save
-    
+
     bar = Parse.get("TestSave",foo.id) # pull it from the server
     assert_equal bar["name"], 'john'
     bar["name"] = 'dave'
     bar.save
-    
+
     bat = Parse.get("TestSave",foo.id)
     assert_equal bar["name"], 'dave'
   end
-  
+
   def test_destroy
     d = Parse::Object.new "toBeDeleted"
     d["foo"] = "bar"
     d.save
     d.parse_delete
-    
+
     assert_equal d.keys.length, 0
   end
 end
