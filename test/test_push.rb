2a3f3cb0da3c1be4786f2e321056969bd06062d4
Tikhon Bernstam <tikhon@gmail.com>
2012-10-08 21:45:49 -0700

    add second test around push to query -- using where should remove channel. not using where should not

--- test/test_push.rb
+++ test/test_push.rb
@@ -6,7 +6,28 @@ class TestPush < Test::Unit::TestCase
     Parse.init
   end
 
-  def test_save
+  def test_save_without_where
+    data = {:foo => 'bar',
+            :alert => 'message'}
+    pf_push = Parse::Push.new(data, "some_chan")
+    pf_push.type = 'ios'
+
+    query = Parse::Query.new(Parse::Protocol::CLASS_INSTALLATION).eq('deviceToken', 'baz')
+
+    Parse::Client.any_instance.expects(:request).with do |uri, method, body, query|
+      hash = JSON.parse(body)
+      assert_equal :post, method
+      assert has_entries('type' => 'ios', 'channel' => "some_chan").matches?([hash])
+      assert has_entries('foo' => 'bar', 'alert' => 'message').matches?([hash['data']])
+      assert_nil query
+      true
+    end.returns({}.to_json)
+
+    pf_push.save
+  end
+
+
+  def test_save_with_where_removes_channel
     data = {:foo => 'bar',
             :alert => 'message'}
     pf_push = Parse::Push.new(data, "some_chan")
@@ -17,16 +38,12 @@ class TestPush < Test::Unit::TestCase
 
     Parse::Client.any_instance.expects(:request).with do |uri, method, body, query|
       hash = JSON.parse(body)
-      assert_equal :post, method
-      assert has_entries('type' => 'ios', 'channel' => "some_chan").matches?([hash])
-      assert has_entries('foo' => 'bar', 'alert' => 'message').matches?([hash['data']])
+      assert_false has_entries('channel' => "some_chan").matches?([hash])
       assert has_entries('deviceToken' => 'baz').matches?([hash['where']])
-      assert_nil query
       true
     end.returns({}.to_json)
 
     pf_push.save
   end
-
 
 end
