615125d665b542cdaf4744c7f0a1a3e0c122b80f
Eric Jensen <ericcj@gmail.com>
2013-01-10 12:14:13 -0500

    don't send sessionToken in body of user when saving since it's invalid

--- test/test_user.rb
+++ test/test_user.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
 class TestUser < Test::Unit::TestCase
-  
+
   def setup
     Parse.init
   end
@@ -19,42 +19,45 @@ class TestUser < Test::Unit::TestCase
       assert_equal user[Parse::Protocol::KEY_CREATED_AT].class, String
     end
   end
-  
+
   def test_user_login
     #VCR.use_cassette('test_user_login', :record => :new_episodes) do
       u = "alan" + rand(10000000000000).to_s
       data = {
-        :username => u, 
+        :username => u,
         :password => "secret"
       }
 
       user = Parse::User.new(data)
 
       user.save
-      
+
       assert_equal user["username"], u
       assert_equal user[Parse::Protocol::KEY_USER_SESSION_TOKEN].class, String
-      
+
       login = Parse::User.authenticate(u, "secret")
-      
+
       assert_equal login["username"], user["username"]
       assert_equal login["sessionToken"].class, String
+
+      user = user.pointer.get
+      user.save
     #end
   end
 
   def test_reset_password
-      u =  "alan" + rand(10000000000000).to_s + "@gmail.com" 
+      u =  "alan" + rand(10000000000000).to_s + "@gmail.com"
       data = {
-        :username => u, 
+        :username => u,
         :password => "secret"
       }
 
       user = Parse::User.new(data)
 
       user.save
-      
+
       reset_password = Parse::User.reset_password(u)
-      
+
       assert true
   end
 
