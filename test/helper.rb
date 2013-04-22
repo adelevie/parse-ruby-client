8233ed5f6c123cf7303d30d029ee79d6f54f3faf
Eric Jensen <ericcj@gmail.com>
2013-04-22 09:48:52 -0400

    filter sensitive headers from vcr

--- test/helper.rb
+++ test/helper.rb
@@ -13,15 +13,28 @@ require 'mocha'
 require 'vcr'
 require 'webmock/test_unit'
 
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'parse-ruby-client'
+
 VCR.configure do |c|
   c.cassette_library_dir = 'fixtures/vcr_cassettes'
   c.hook_into :webmock # or :fakeweb
   c.allow_http_connections_when_no_cassette = true
-end
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'parse-ruby-client'
+  def filter_sensitive_header(c, header)
+    c.filter_sensitive_data("<#{header}>") do |interaction|
+      if v = interaction.request.headers.detect{|k,_| k.casecmp(header) == 0}
+        v.last.first
+      end
+    end
+  end
+
+  filter_sensitive_header(c, Parse::Protocol::HEADER_APP_ID)
+  filter_sensitive_header(c, Parse::Protocol::HEADER_API_KEY)
+  filter_sensitive_header(c, Parse::Protocol::HEADER_MASTER_KEY)
+  filter_sensitive_header(c, Parse::Protocol::HEADER_SESSION_TOKEN)
+end
 
 class Test::Unit::TestCase
 end
