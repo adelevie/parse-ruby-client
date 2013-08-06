35f07c401591335ccec6b5d20dbafcb8504416cd
Jodi J. Showers <jodi@homestars.com>
2013-08-05 21:22:23 -0400

    added installation.get - which fails as it is getting an unauthorized http error - likely a setup issue with test\?

--- lib/parse/installation.rb
+++ lib/parse/installation.rb
@@ -11,13 +11,21 @@ module Parse
     attr_accessor :channels
     attr_accessor :parse_object_id
 
+    def self.get(installation_id)
+      installation = Installation.new(installation_id)
+
+      if response = Parse.client.request(installation.uri, :get, nil, nil)
+        installation.parse Parse.parse_json(nil, response)
+      end
+
+      installation       
+    end
+
     def initialize(parse_object_id = nil)
       @parse_object_id = parse_object_id
       @device_type = nil
       @device_token = nil
       @channels = []
-
-      puts "@parse_object_id == #{@parse_object_id}"
     end
 
     def uri
@@ -35,8 +43,12 @@ module Parse
     # Write the current state of the local object to the API.
     # If the object has never been saved before, this will create
     # a new object, otherwise it will update the existing stored object.
+
+
+    # this method fails unless the deviceType is registered with Apple
+    # so I don't know how to write a passing test..JJS
     def save
-      body = { :device_type => @device_type, :device_token => @device_token, :channels => @channels }
+      body = { :deviceType => @device_type, :deviceToken => @device_token, :channels => @channels }
 
       if response = Parse.client.request(uri, method, body.to_json, nil)
         # array operations can return mutated view of array which needs to be parsed
@@ -44,19 +56,6 @@ module Parse
       end
 
       self      
-    end
-
-    def self.get(installation_id)
-      installation = Installation.new(installation_id)
-
-      puts "installation.uri == #{installation.uri}"
-
-      if response = Parse.client.request(installation.uri, :get, nil, nil)
-        # array operations can return mutated view of array which needs to be parsed
-        parse Parse.parse_json(nil, response)
-      end
-
-      self       
     end
 
   end
