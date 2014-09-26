3baf583ddc91660d846f4f00b9b70759ac9d1b68
Chris Bielinski <chris@shadow.io>
2014-09-26 12:34:51 -0700

    add #find method to Parse::Model

--- lib/parse/model.rb
+++ lib/parse/model.rb
@@ -1,8 +1,14 @@
 # -*- encoding : utf-8 -*-
 module Parse
   class Model < Parse::Object
+
     def initialize(data=nil)
       super(self.class.to_s,data)
     end
+
+    def self.find(object_id)
+      self.new Parse::Query.new(self.to_s).eq('objectId',object_id).first
+    end
+
   end
 end
