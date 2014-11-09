26499a65f6b6ea9ac33066104dee8120e4639fd3
Fabio Milano <fabiomassimomilano@gmail.com>
2014-11-09 19:21:11 +0100

    Removed superfluous trailing spaces
    Removed trailing superfluous spaces

--- test/test_push.rb
+++ test/test_push.rb
@@ -1,14 +1,14 @@
 require 'helper'
 
 class TestPush < ParseTestCase
-    
+
   def test_save_without_where
     data = {:foo => 'bar',
-        :alert => 'message'}
+            :alert => 'message'}
     pf_push = Parse::Push.new(data, "some_chan")
-        
+
     query = Parse::Query.new(Parse::Protocol::CLASS_INSTALLATION).eq('deviceToken', 'baz')
-        
+
     Parse::Client.any_instance.expects(:request).with do |uri, method, body, query|
       hash = JSON.parse(body)
       assert_equal :post, method
@@ -17,31 +17,31 @@ class TestPush < ParseTestCase
       assert_nil query
       true
     end.returns({}.to_json)
-        
+
     pf_push.save
   end
-    
-    
+
+
   def test_save_with_channels_removes_channel
     data = {:foo => 'bar', :alert => 'message'}
     pf_push = Parse::Push.new(data, "some_chan")
     pf_push.type = 'ios'
-        
+
     query = Parse::Query.new(Parse::Protocol::CLASS_INSTALLATION).eq('deviceToken', 'baz')
     pf_push.where = query.where
 
     pf_push.channels = ["foo", "bar"]
-        
-        
+
+
     Parse::Client.any_instance.expects(:request).with do |uri, method, body, query|
       hash = JSON.parse(body)
       assert_false has_entries('channel' => "some_chan").matches?([hash])
       assert has_entries('deviceToken' => 'baz', 'deviceType' => 'ios').matches?([hash['where']])
       true
     end.returns({}.to_json)
-        
+
     pf_push.save
   end
-    
+
 end
 
