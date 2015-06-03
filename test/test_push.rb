635345df99d0582c77dd439e8f603eb404769240
Fabio Milano <fabio@touchwonders.com>
2015-06-03 23:27:26 +0200

    Updated push test accordingly to Parse push documentation

--- test/test_push.rb
+++ test/test_push.rb
@@ -13,7 +13,7 @@ class TestPush < ParseTestCase
     Parse::Client.any_instance.expects(:request).with do |uri, method, body, query|
       hash = JSON.parse(body)
       assert_equal :post, method
-      assert has_entries('type' => 'ios', 'channel' => "some_chan").matches?([hash])
+      assert has_entries('channel' => "some_chan").matches?([hash])
       assert has_entries('foo' => 'bar', 'alert' => 'message').matches?([hash['data']])
       assert_nil query
       true
@@ -23,19 +23,21 @@ class TestPush < ParseTestCase
   end
 
 
-  def test_save_with_where_removes_channel
-    data = {:foo => 'bar',
-            :alert => 'message'}
-    pf_push = Parse::Push.new(data, "some_chan", client = @client)
+  def test_save_with_channels_removes_channel
+    data = {:foo => 'bar', :alert => 'message'}
+    pf_push = Parse::Push.new(data, "some_chan",client = @client)
     pf_push.type = 'ios'
 
     query = Parse::Query.new(Parse::Protocol::CLASS_INSTALLATION, client = @client).eq('deviceToken', 'baz')
     pf_push.where = query.where
 
+    pf_push.channels = ["foo", "bar"]
+
+
     Parse::Client.any_instance.expects(:request).with do |uri, method, body, query|
       hash = JSON.parse(body)
       assert_false has_entries('channel' => "some_chan").matches?([hash])
-      assert has_entries('deviceToken' => 'baz').matches?([hash['where']])
+      assert has_entries('deviceToken' => 'baz', 'deviceType' => 'ios').matches?([hash['where']])
       true
     end.returns({}.to_json)
 
