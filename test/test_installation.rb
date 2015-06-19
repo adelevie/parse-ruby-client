5b452da367cbe4c25850e63e0650e7f94ea846ea
Andrey Krivko <jastkand@gmail.com>
2015-06-19 19:08:13 +0600

    add missing fileds to installation, update documentation

--- test/test_installation.rb
+++ test/test_installation.rb
@@ -1,6 +1,29 @@
 require 'helper'
 
 class TestInstallation < ParseTestCase
+  def test_create_installation_with_valid_data
+    VCR.use_cassette('test_create_valid_installation') do
+      installation = @client.installation.tap do |i|
+        i.device_token = '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
+        i.device_type = 'ios'
+      end.save
+
+      assert_equal installation['createdAt'].class, String
+      assert_equal installation['objectId'].class, String
+    end
+  end
+
+  def test_create_installation_with_invalid_data
+    VCR.use_cassette('test_create_invalid_installation') do
+      installation = @client.installation.tap do |i|
+        i.device_token = '123'
+        i.device_type = 'ios'
+      end
+
+      assert_raises(Parse::ParseProtocolError) { installation.save }
+    end
+  end
+
   def test_retrieving_installation_data
     installation_data = {
       "appIdentifier"=>"net.project_name",
@@ -19,25 +42,25 @@ class TestInstallation < ParseTestCase
     }
 
     VCR.use_cassette('test_get_installation') do
-      installation = Parse::Installation.get("987", client = @client)
-      assert_equal installation_data, installation
+      data = Parse::Installation.get('987', @client)
+      assert_equal installation_data, data
     end
   end
 
   def test_changing_channels
-    installation = Parse::Installation.new("987", client = @client)
+    installation = Parse::Installation.new('987', @client)
     installation.channels = ["", "my-channel"]
     assert_equal ["", "my-channel"], installation["channels"]
   end
 
   def test_changing_badges
-    installation = Parse::Installation.new("987", client = @client)
+    installation = Parse::Installation.new('987', @client)
     installation.badge = 5
     assert_equal 5, installation["badge"]
   end
 
   def test_updating_installation_data
-    installation = Parse::Installation.new("987", client = @client)
+    installation = Parse::Installation.new('987', @client)
     installation.channels = ["", "my-channel"]
     installation.badge = 5
 
